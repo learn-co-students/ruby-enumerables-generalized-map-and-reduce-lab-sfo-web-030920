641fba367fb9566b1c07b9a6cafff7c49a8f4868
Tisdale Fry <tisdalefry@gmail.com>
2020-03-04 21:11:04 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,40 @@
 # Your Code Here
+require 'pry'
+
+def map(array)
+  idx = 0
+  new_arr = []
+  while idx < array.length do
+    new_arr.push(yield(array[idx]))
+    idx += 1
+  end
+  new_arr
+end
+
+def reduce(array, sv = nil)
+  
+  if sv
+    sum = sv
+    idx = 0
+  else
+    sum = array[0]
+    idx = 1
+  end
+  while idx < array.length do
+    sum = yield(sum, array[idx])
+    idx += 1
+  end
+  sum
+end
+
+
+
+
+
+
+
+
+
+
+
+
